9dd87a1292606292331c16180575dd585b7a7613
jackra1n <lok7ok@gmail.com>
2023-06-02 08:59:18 +0200

    wip. add shadcn-ui

---
+++ src/Download.tsx
@@ -0,0 +1,10 @@
+import { Input } from "@/components/ui/input"
+
+function Download() {
+
+  return (
+    <Input placeholder="URL" />
+  )
+}
+
+export default Download
--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import Header from './components/header/Header.tsx'
-import Download from './components/download/Download.tsx'
-import './index.css'
+import Header from './Header.tsx'
+import Download from './Download.tsx'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
