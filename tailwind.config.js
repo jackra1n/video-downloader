9ee18c4a032f94c83177952de6edef280930bd6b
jackra1n <lok7ok@gmail.com>
2023-06-02 15:37:07 +0200

    remove montserrat

--- tailwind.config.js
+++ tailwind.config.js
@@ -14,9 +14,6 @@ module.exports = {
       },
     },
     extend: {
-      fontFamily: {
-        sans: ["Montserrat Variable", "sans-serif"]
-      },
       colors: {
         border: "hsl(var(--border))",
         input: "hsl(var(--input))",
