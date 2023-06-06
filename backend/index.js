558c6832a41da8da9358f2f30941eda9e7776de8
jackra1n <lok7ok@gmail.com>
2023-06-06 12:24:46 +0200

    first backend examples

--- backend/index.js
+++ backend/index.js
@@ -1,12 +1,28 @@
 
 const express = require('express');
-const youtubedl = require('youtube-dl-exec')
+const ytdl = require('ytdl-core')
 
 const app = express();
 
+app.get("/download", async (req, res) => {
+	if (req.query.url == null) {
+		res.send("No url query parameter provided");
+		return;	
+	}
+	const v_id = req.query.url.split('v=')[1];
+    const info = await ytdl.getInfo(req.query.url);
+	console.log(info);
+	console.log(info.formats);
+	res.send({
+        info: info.formats.sort((a, b) => {
+            return a.mimeType < b.mimeType;
+        }),
+	});
+});
+
 app.get('/files', (req, res) => {
 	console.log("TEST DATA");
-	res.json({ "files" : {}});
+	res.json({ "files" : { "name" : "test"}});
 })
 
 app.listen(3000, () => { console.log('Server is running...') })
